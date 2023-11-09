a7fa9e7048ee270e998d5afcc587922c0ef31a83
BrianPizz <bpizzimentijr@outlook.com>
2023-11-08 19:37:02 -0800

    contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div name="contact" className='w-full h-screen bg-gradient-to-t from-black to-slate-700'>
+        Contact
+    </div>
+  )
+}
+
+export default Contact
