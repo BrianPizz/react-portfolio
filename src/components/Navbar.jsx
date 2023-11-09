a115a1f649028fff2856d07cacd14b3c1d3a5289
BrianPizz <bpizzimentijr@outlook.com>
2023-11-08 21:27:59 -0800

    format scroll and mobile nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,6 @@ import { Link } from 'react-scroll'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
-  // set home as the active link
-  const [activeLink, setActiveLink] = useState('Home');
 
   const handleNavClick = () => setNav(!nav);
 
@@ -51,36 +49,31 @@ const Navbar = () => {
 
       {/* Mobile menu */}
       <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-black flex flex-col justify-center items-center'}>
-        <li
-          className={`py-6 text-4xl ${activeLink === 'Home' ? 'active-link' : ''}`}
-          onClick={() => handleNavLinkClick('Home')}
-        >
-          Home
-        </li>
-        <li
-          className={`py-6 text-4xl ${activeLink === 'About' ? 'active-link' : ''}`}
-          onClick={() => handleNavLinkClick('About')}
-        >
-          About
-        </li>
-        <li
-          className={`py-6 text-4xl ${activeLink === 'Portfolio' ? 'active-link' : ''}`}
-          onClick={() => handleNavLinkClick('Portfolio')}
-        >
-          Portfolio
-        </li>
-        <li
-          className={`py-6 text-4xl ${activeLink === 'Resume' ? 'active-link' : ''}`}
-          onClick={() => handleNavLinkClick('Resume')}
-        >
-          Resume
-        </li>
-        <li
-          className={`py-6 text-4xl ${activeLink === 'Contact' ? 'active-link' : ''}`}
-          onClick={() => handleNavLinkClick('Contact')}
-        >
-          Contact
-        </li>
+        <Link onClick={handleNavClick} to='home' activeClass='active' smooth={true} spy={true} duration={500}>
+          <li className='py-6 text-4xl'>
+            Home
+          </li>
+        </Link>
+        <Link onClick={handleNavClick} to='about' activeClass='active' smooth={true} spy={true} duration={500}>
+          <li className='py-6 text-4xl'>
+            About
+          </li>
+        </Link>
+        <Link onClick={handleNavClick} to='portfolio' activeClass='active' smooth={true} spy={true} duration={500}>
+          <li className='py-6 text-4xl'>
+            Portfolio
+          </li>
+        </Link>
+        <Link onClick={handleNavClick} to='resume' activeClass='active' smooth={true} spy={true} duration={500}>
+          <li className='py-6 text-4xl'>
+            Resume
+          </li>
+        </Link>
+        <Link onClick={handleNavClick} to='contact' activeClass='active' smooth={true} spy={true} duration={500}>
+          <li className='py-6 text-4xl'>
+            Contact
+          </li>
+        </Link>
       </ul>
     </div>
   );
