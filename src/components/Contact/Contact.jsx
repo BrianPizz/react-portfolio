763be83f6c06d928eb5b921eea650e5ac69e3c5b
BrianPizz <bpizzimentijr@outlook.com>
2023-11-22 08:48:08 -0800

    contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
   return (
     <div name="contact" className='w-full h-screen bg-black flex flex-col items-center pt-20'>
       <p className='text-4xl text-rose-300 mb-4'>Contact</p>
-      <form action="" className='flex flex-col rounded-lg w-6/12 p-5 bg-slate-100'>
+      <form method='POST' action="https://getform.io/f/9e07be95-81c5-4728-ac4d-019c3f8cfc12" className='flex flex-col rounded-lg w-6/12 p-5 bg-slate-100'>
         <input required className='rounded p-1 shadow-lg' type="text" placeholder='Enter Your Name' name='name' />
         <input required className='my-4 p-1 rounded shadow-lg' type="email" placeholder='Enter Your Email' name='email' />
         <textarea required className='p-1 rounded shadow-lg' placeholder='Enter Your message' name="message" rows="10"></textarea>
